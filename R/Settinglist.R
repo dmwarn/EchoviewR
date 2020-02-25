f9dfccbe64ceeb32aa3e1c11c23ec938a26c91ba
dmwarn <dmwarner@usgs.gov>
2020-02-25 12:28:03 -0500

    Many changes

---
+++ R/Settinglist.R
@@ -0,0 +1,15 @@
+#' Settings for Sv minus noise variables
+#' 
+#' Data tab
+#' ApplyMaximumDataThreshold | false | 
+#' ApplyMinimumDataThreshold | false | 
+#' ApplyMinimumTsThreshold | true | 
+#' ApplyTimeVariedDataThreshold | false | 
+#' LockMaximumDataThresholdToMaximumColor | true | 
+#' LockMinimumDataThresholdToMinimumColor | true | 
+#' MaximumDataThreshold | 99.0 | 
+#' MinimumDataThreshold | -70.0 | 
+#' MinimumTSThreshold | -66.0 | 
+#' MinimumTSThresholdCalibrationDifferenceOffset | 0.0 | 
+#' ThresholdedValuesSetTo | Zero | 
+#' TimeVariedDataThreshold | -999.0
